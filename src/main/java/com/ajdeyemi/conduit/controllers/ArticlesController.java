e1f1ebbd0563d013fdbeb995d4933fb224751bed
adeyemi <ajdeyemi@adeyemis-air.home>
2024-01-31 23:12:02 -0500

    articles controller

---
+++ src/main/java/com/ajdeyemi/conduit/controllers/ArticlesController.java
@@ -0,0 +1,25 @@
+package com.ajdeyemi.conduit.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ajdeyemi.conduit.models.Articles;
+import com.ajdeyemi.conduit.services.ArticlesService;
+
+@RestController
+public class ArticlesController{
+
+    @Autowired
+    ArticlesService articlesService;
+
+    @GetMapping("/articles")
+    public Page<Articles> getArticles(@RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "20") int limit
+    ){
+        return articlesService.geArticles(page, limit);
+    }
+
+}
