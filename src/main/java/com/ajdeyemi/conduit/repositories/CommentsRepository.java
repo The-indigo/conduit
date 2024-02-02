bf5bfd5fe4cf592d74bdc3ecfe6df0d16eed3a10
The-indigo <adepojuadeyemi11@gmail.com>
2024-02-02 17:24:16 -0500

    Delet comment service

--- src/main/java/com/ajdeyemi/conduit/repositories/CommentsRepository.java
+++ src/main/java/com/ajdeyemi/conduit/repositories/CommentsRepository.java
@@ -3,13 +3,10 @@ package com.ajdeyemi.conduit.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.ajdeyemi.conduit.models.Comments;
-import com.ajdeyemi.conduit.models.Followers;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
     List<Comments> findByArticle(long id);
-      @Query("SELECT article, user, comment FROM Comments  WHERE article = ?1")
-    Comments findCommentByArticle(long id);
+
 }
