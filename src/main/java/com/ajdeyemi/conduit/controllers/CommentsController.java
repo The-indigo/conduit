5441eb3671ee85d4d8b1d9f71393945599d755fe
The-indigo <adepojuadeyemi11@gmail.com>
2024-02-10 23:00:50 -0500

    delete comments controller

--- src/main/java/com/ajdeyemi/conduit/controllers/CommentsController.java
+++ src/main/java/com/ajdeyemi/conduit/controllers/CommentsController.java
@@ -2,6 +2,8 @@ package com.ajdeyemi.conduit.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,12 +24,29 @@ public class CommentsController {
     public ResponseEntity<?>addComment(@PathVariable String slug,@RequestBody CommentBody commentBody){
         try{
             return ResponseEntity.ok().body(commentsService.addComment(slug, commentBody.body()));
-
         }catch(Exception e){
             return GetError.getError(e);
         }
-        
     }
+    @GetMapping("")
+    public ResponseEntity<?>getComments(@PathVariable String slug){
+        try{
+            return ResponseEntity.ok().body(commentsService.getArticleComments(slug));
+        }catch(Exception e){
+            return GetError.getError(e);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?>deleteComment(@PathVariable String slug, @PathVariable long id){
+        try{
+            return ResponseEntity.ok().body(commentsService.deleteComment(slug, id));
+        }catch(Exception e){
+            return GetError.getError(e);
+        }
+    }
+
+
+
     
 }
 
